aa70f50bd3f76e46e548cadaabbc3ea17bc52cf3
Noah G <greffnoah@gmail.com>
2026-03-03 18:58:16 +0100

    added new files

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -1,36 +1,25 @@
 import connexion
-import six
+from swagger_server.models.student import Student
+from swagger_server.service.student_service import *
+from swagger_server.service.student_service import add, get_by_id, get_all, delete
 
-from swagger_server.models.student import Student  # noqa: E501
-from swagger_server import util
 
-
-def add_student(body):  # noqa: E501
+def add_student(body=None):  # noqa: E501
     """Add a new student
 
-    Adds a student item to the system # noqa: E501
+    Adds an item to the system # noqa: E501
 
     :param body: Student item to add
     :type body: dict | bytes
 
-    :rtype: int
+    :rtype: float
     """
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        student_id = add(body)
+        return student_id, 201
 
-
-def delete_student(student_id):  # noqa: E501
-    """Deletes a student
-
-    Delete a single student by ID # noqa: E501
-
-    :param student_id: ID of student to delete
-    :type student_id: int
-
-    :rtype: None
-    """
-    return 'do some magic!'
+    return 'Invalid input', 400
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,7 +32,11 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    student = get_by_id(student_id)
+    if student:
+        return student, 200
+
+    return 'Student not found', 404
 
 
 def get_students():  # noqa: E501
@@ -51,7 +44,27 @@ def get_students():  # noqa: E501
 
     Returns a list of all students # noqa: E501
 
-
     :rtype: List[Student]
     """
-    return 'do some magic!'
+    students = get_all()
+    if students:
+        return students, 200
+
+    return 'No students found', 404
+
+
+def delete_student(student_id):  # noqa: E501
+    """Deletes a student
+
+    Delete a single student by ID # noqa: E501
+
+    :param student_id: ID of student to delete
+    :type student_id: int
+
+    :rtype: None
+    """
+    result = delete(student_id)
+    if result:
+        return 'Student deleted successfully', 200
+
+    return 'Student not found', 404
